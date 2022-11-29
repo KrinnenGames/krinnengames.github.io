5a20a8b7dc7048eeb530e33d9f7acaef63d5b655
Niels Krijnen <niels.krijnen@outlook.com>
2022-11-29 22:20:19 +0100

    Update

---
+++ Scripts/searchstorage.js
@@ -0,0 +1,69 @@
+var input = document.getElementById("nameinput");
+var table = document.getElementById("searchtable");
+var tr = table.getElementsByTagName("tr");
+var option_brand = document.getElementById("brand_select");
+var filter = input.value.toUpperCase();
+var txtValue_0, txtValue_1, td_0, td_1, i;
+
+function namesearch() {
+  filter = input.value.toUpperCase();
+  for (i = 0; i < tr.length; i++) {
+    td_0 = tr[i].getElementsByTagName("td")[0];
+    td_1 = tr[i].getElementsByTagName("td")[1];
+    if (td_0 && td_1) {
+      txtValue_0 = td_0.textContent || td_0.innerText;
+      txtValue_1 = td_1.textContent || td_1.innerText;
+      if (txtValue_0.toUpperCase().indexOf(filter) > -1) {
+        if (option_brand.value == "All") {
+          tr[i].style.display = "";
+        } else if (option_brand.value == "Biostar" && txtValue_1 == "Biostar") {
+          tr[i].style.display = "";
+        } else if (option_brand.value == "ASUS" && txtValue_1 == "ASUS") {
+          tr[i].style.display = "";
+        }
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function brand_select() {
+  if (option_brand.value == "All") {
+    for (i = 0; i < tr.length; i++) {
+      tr[i].style.display = "";
+    }
+    namesearch();
+  }
+  filter = input.value.toUpperCase();
+  if (option_brand.value == "Biostar") {
+    for (i = 0; i < tr.length; i++) {
+      td_0 = tr[i].getElementsByTagName("td")[0];
+      td_1 = tr[i].getElementsByTagName("td")[1];
+      if (td_0 && td_1) {
+        txtValue_0 = td_0.textContent || td_0.innerText;
+        txtValue_1 = td_1.textContent || td_1.innerText;
+        if (txtValue_1 == "Biostar" && txtValue_0.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  if (option_brand.value == "ASUS") {
+    for (i = 0; i < tr.length; i++) {
+      td_0 = tr[i].getElementsByTagName("td")[0];
+      td_1 = tr[i].getElementsByTagName("td")[1];
+      if (td_0 && td_1) {
+        txtValue_0 = td_0.textContent || td_0.innerText;
+        txtValue_1 = td_1.textContent || td_1.innerText;
+        if (txtValue_1 == "ASUS" && txtValue_0.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+}
